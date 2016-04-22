0eb2a8566cba71b163a9b5bf884949300822889c
Federico Teotini <templarblack.fed03@gmail.com>
2016-04-22 23:25:12 +0200

    Removed auto import of pikaday assets

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/pikaday/pikaday.js');
-    app.import(app.bowerDirectory + '/pikaday/css/pikaday.css');
+    // app.import(app.bowerDirectory + '/pikaday/pikaday.js');
+    // app.import(app.bowerDirectory + '/pikaday/css/pikaday.css');
   }
 };
